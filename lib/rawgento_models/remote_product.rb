fbcbcfb23a61bbe8dc37e5d5033719d75d236d33
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-01-22 15:08:45 +0100

    Add RemoteProduct#linked and RemoteProduct#unlinked

--- lib/rawgento_models/remote_product.rb
+++ lib/rawgento_models/remote_product.rb
@@ -1,6 +1,12 @@
 module RawgentoModels
   class RemoteProduct < ActiveRecord::Base
     belongs_to :local_product
+    def self.linked
+      self.where.not(local_product: nil)
+    end
+    def self.unlinked
+      self.where(local_product: nil)
+    end
   end
 end
 
