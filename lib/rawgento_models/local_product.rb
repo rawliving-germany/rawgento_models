465e1bd3f984c053d9a15b5366e0cc9228de997d
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-01-25 12:22:39 +0100

    add scopes and link_suggestions to LocalProduct

--- lib/rawgento_models/local_product.rb
+++ lib/rawgento_models/local_product.rb
@@ -2,5 +2,30 @@ module RawgentoModels
   class LocalProduct < ActiveRecord::Base
     has_one :remote_product
     has_many :stock_items
+
+    default_scope { order("name ASC") }
+
+    scope :linked, -> {
+      joins("LEFT OUTER JOIN " +
+            "  remote_products " +
+            "  ON remote_products.local_product_id = local_products.id").where("remote_products.id IS NOT null")
+    }
+    # scope :with_photos, -> { joins(:photos).distinct }
+
+    scope :unlinked, -> {
+      joins("LEFT OUTER JOIN " +
+            "  remote_products " +
+            "  ON remote_products.local_product_id = local_products.id").where("remote_products.id IS null")
+    }
+    # City.includes(:photos).where(photos: { city_id: nil }) # slower
+    scope :supplied_by, ->(supplier_name) { where(supplier: supplier_name) }
+
+    def link_suggestions
+      guesses = []
+      guesses << RemoteProduct.supplied_by(self.supplier).where(name: self.name).to_a
+      guesses << RemoteProduct.supplied_by(self.supplier).where("lower(name) like ?",
+                                     "%#{self.name.downcase[0..self.name.size/2]}%").to_a
+      guesses.flatten
+    end
   end
 end
