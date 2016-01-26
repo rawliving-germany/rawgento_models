ad816b2d0381f6d644d6f1280561128a7b7b01f9
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-01-26 09:42:59 +0100

    add RemoteProduct#supplied_by scope

--- lib/rawgento_models/remote_product.rb
+++ lib/rawgento_models/remote_product.rb
@@ -1,9 +1,13 @@
 module RawgentoModels
   class RemoteProduct < ActiveRecord::Base
     belongs_to :local_product
+
+    scope :supplied_by, ->(supplier_name) { where(supplier: supplier_name) }
+
     def self.linked
       self.where.not(local_product: nil)
     end
+
     def self.unlinked
       self.where(local_product: nil)
     end
