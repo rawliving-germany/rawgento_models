d7486906aa2e780af49574786a7bd7b86b0bc660
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-02-09 12:26:29 +0100

    pretty evil time travel migration change.

--- db/migrate/201601221500_create_orders.rb
+++ db/migrate/201601221500_create_orders.rb
@@ -1,14 +1,14 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |p|
-      p.string  "state"
+      p.string  "state", default: :new
       p.datetime "created_at", null: false
       p.datetime "updated_at", null: false
     end
 
     create_table :order_items do |p|
       p.references :order, null: false
-      p.string  "state"
+      p.string  "state", default: :new
       p.integer  "num_wished"
       p.integer  "num_ordered"
       p.references :remote_product
