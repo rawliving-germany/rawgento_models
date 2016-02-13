247ac6aa121baa77a0a99c6db05d1522fdff0672
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-02-13 19:37:33 +0100

    add OrderItem#all_ordered? out_of_stock?, remote_product_id and processible scope

--- lib/rawgento_models/order_item.rb
+++ lib/rawgento_models/order_item.rb
@@ -4,5 +4,19 @@ module RawgentoModels
     belongs_to :local_product
 
     delegate :name, to: :local_product
+
+    scope :processible, -> { where("num_wished > 0") }
+
+    def remote_product_id
+      local_product.remote_product.try(:product_id)
+    end
+
+    def all_ordered?
+      num_wished == num_ordered
+    end
+
+    def out_of_stock?
+      num_ordered == 0
+    end
   end
 end
