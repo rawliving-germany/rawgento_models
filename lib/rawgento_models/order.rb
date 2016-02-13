03357fbb160b80a9823bf4cd27436a6f7da0fba4
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-02-13 19:36:50 +0100

    add Order#queued and fix Order#current

--- lib/rawgento_models/order.rb
+++ lib/rawgento_models/order.rb
@@ -1,6 +1,10 @@
 module RawgentoModels
   class Order < ActiveRecord::Base
     has_many :order_items
-    scope :current, -> { find_by(state: "new") }
+    scope :queued, -> { where(state: "queued") }
+
+    def self.current
+      find_by(state: "new")
+    end
   end
 end
