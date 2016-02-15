f3b7fe84a585a21b2b9887c740d11fa5298a47b1
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-02-15 15:57:45 +0100

    add Supplier#local_products and Supplier#remote_products

--- lib/rawgento_models/supplier.rb
+++ lib/rawgento_models/supplier.rb
@@ -1,6 +1,6 @@
 module RawgentoModels
   class Supplier < ActiveRecord::Base
-    # has_many :local_products
-    # has_many :remote_products
+    has_many :local_products
+    has_many :remote_products
   end
 end
