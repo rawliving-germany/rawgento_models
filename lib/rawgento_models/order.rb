4cb70fb49e483845619611845f6f5ac9e1ff5deb
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-05-27 12:16:42 +0200

    join Order#order_items, will hide hidden local_products order_items

--- lib/rawgento_models/order.rb
+++ lib/rawgento_models/order.rb
@@ -1,6 +1,6 @@
 module RawgentoModels
   class Order < ActiveRecord::Base
-    has_many :order_items
+    has_many :order_items, -> { joins(:local_product) }
     scope :queued, -> { where(state: "queued") }
 
     def self.current
