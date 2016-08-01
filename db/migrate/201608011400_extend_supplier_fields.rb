4f201035d68e317e8e78d791150ee9a365fce572
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-08-01 16:24:14 +0200

    In latest migration hot-rename Supplier#supplier_order_info to #order_info

--- db/migrate/201608011400_extend_supplier_fields.rb
+++ db/migrate/201608011400_extend_supplier_fields.rb
@@ -1,7 +1,7 @@
 class ExtendSupplierFields < ActiveRecord::Migration
   def change
     add_column :suppliers, :delivery_time_days,  :integer
-    add_column :suppliers, :supplier_order_info, :text
+    add_column :suppliers, :order_info,          :text
     add_column :suppliers, :minimum_order_value, :decimal
   end
 end
