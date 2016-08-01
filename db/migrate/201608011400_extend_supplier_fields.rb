85a5fb1b36e6774ab48da67d14a4ab3ed8baeb2c
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-08-01 14:58:40 +0200

    Extend supplier by additional fields (closes #1)
    New fields are minimum_order_value, supplier_order_info and
    delivery_time_days.

---
+++ db/migrate/201608011400_extend_supplier_fields.rb
@@ -0,0 +1,8 @@
+class ExtendSupplierFields < ActiveRecord::Migration
+  def change
+    add_column :suppliers, :delivery_time_days,  :integer
+    add_column :suppliers, :supplier_order_info, :text
+    add_column :suppliers, :minimum_order_value, :decimal
+  end
+end
+
